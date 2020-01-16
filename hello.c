bc0d5cbd26e500f9e609ccf2cab64c327f590e7f
aalkindi4 <59976000+aalkindi4@users.noreply.github.com>
2020-01-16 14:37:19 -0600

    hello
    A simple name and major

---
+++ hello.c
@@ -0,0 +1,22 @@
+/*
+
+Hack 1 session
+Name: Abdullah Alkindi
+Date: 2020/01/16
+
+*/
+
+#include<stdio.h>
+
+
+int main (void) { 
+
+    
+    printf("Abdullah Alkindi\n");
+    printf("Civil Engineering\n");
+
+
+    return 0;
+
+}
+
